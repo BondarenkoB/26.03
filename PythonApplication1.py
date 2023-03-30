77f05bac6cf0cf9241fae46f6f16732332437c4a
bohdan <bohdanstep23@gmail.com>
2023-03-30 23:14:18 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,10 @@
+start = int(input("Введіть початок діапазону: "))
+end = int(input("Введіть кінець діапазону: "))
+
+if start > end:
+    start, end = end, start
+
+for i in range(start, end+1):
+    if i % 2 != 0:
+        print(i)
 
