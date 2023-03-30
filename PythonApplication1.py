a5e8e851012ae05ba5b6469e3ac70abb29d3838f
bohdan <bohdanstep23@gmail.com>
2023-03-30 22:59:41 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,8 @@
+# -*- coding: cp1251 -*- 
+start = int(input("Початок діапазону: "))
+end = int(input("кінець діапазону: "))
+
+for num in range(start, end+1):
+    if num % 7 == 0:
+        print(num)
 
