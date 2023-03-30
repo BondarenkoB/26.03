3e6d6bb77ab6de37978159f7379a979466748108
bohdan <bohdanstep23@gmail.com>
2023-03-30 23:04:46 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,13 @@
+start = int(input("Початок діапазон: "))
+end = int(input("Кiнець діапазону: "))
+
+for num in range(start, end+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("Fizz Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
 
