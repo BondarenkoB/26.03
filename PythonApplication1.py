803f684903123311527fd14aeada7b1f49e58941
bohdan <bohdanstep23@gmail.com>
2023-03-30 23:03:15 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,22 @@
+start = int(input("Початок діапазону: "))
+end = int(input("Кiнець діапазону: "))
+
+print("Усі числа діапазону:")
+for num in range(start, end+1):
+    print(num)
+
+print("Усі числа діапазону за спаданням:")
+for num in range(end, start-1, -1):
+    print(num)
+
+print("Усі числа, кратні 7:")
+for num in range(start, end+1):
+    if num % 7 == 0:
+        print(num)
+
+count = 0
+for num in range(start, end+1):
+    if num % 5 == 0:
+        count += 1
+print("Кількість чисел, кратних 5:", count)
 
